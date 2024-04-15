5dd2312a7f83e2ec0b35d09eba016296281593db
Kolodieiev <valerij.kolodeev@gmail.com>
2024-04-15 23:06:47 +0300

    Update string.h

--- src/screen/resources/string.h
+++ src/screen/resources/string.h
@@ -2,24 +2,25 @@
 #include <Arduino.h>
 #include <pgmspace.h>
 
-const char STR_AUTHOR[]  = "Автор: Kolodieiev";
-const char STR_SELECT[]  = "Вибір";
-const char STR_BACK[]  = "Назад";
-const char STR_START[]  = "Розпочати";
-const char STR_STOP[]  = "Зупинити";
-const char STR_LIGHT[]  = "Підсвітка";
+const char STR_AUTHOR[] = "Автор: Kolodieiev";
+const char STR_SELECT[] = "Вибір";
+const char STR_BACK[] = "Назад";
+const char STR_START[] = "Розпочати";
+const char STR_STOP[] = "Зупинити";
+const char STR_LIGHT[] = "Підсвітка";
 
 // home screen
-const char STR_HOME_HEADER[]  = "Оберіть зі списку";
-//
-const char STR_FLICK_ITEM[]  = "Мерехтіння";
-const char STR_IMAGE_ITEM[]  = "Зображення";
-const char STR_SPRITE_ITEM[]  = "Спрайт";
-const char STR_GAME_ITEM[]  = "Гра";
-//
-const char STR_ITEM_ONE[]  = "Test item 1";
-const char STR_ITEM_TWO[]  = "Test item 2";
+const char STR_HOME_HEADER[] = "Оберіть зі списку";
+const char STR_GAME_ITEM[] = "Гра";
+// splash screen
+const char STR_SD_LBL[] = "Ініціалізація SD";
+const char STR_PSRAM_LBL[] = "Ініціалізація PSRAM";
+const char STR_AUDIO_LBL[] = "Ініціалізація AUDIO";
 
+const char STR_SUCCSESS[] = "Успішно";
+const char STR_FAIL[] = "Невдача";
+
+//
 // const char MON_STR[]  = "Понеділок";
 // const char TUE_STR[]  = "Вівторок";
 // const char WED_STR[]  = "Середа";
