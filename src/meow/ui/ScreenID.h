93e3a95ef5ddf2ffde7dfcca83cd898a026baf5a
Kolodieiev <valerij.kolodeev@gmail.com>
2024-04-15 15:04:00 +0300

    Додано нові id екранів

--- src/meow/ui/ScreenID.h
+++ src/meow/ui/ScreenID.h
@@ -4,6 +4,7 @@
 // ----------------------------------------- Встанови ID екранів
 enum ScreenID : uint8_t
 {
-    ID_SCREEN_HOME = 0,
+    ID_SCREEN_SPLASH = 0,
+    ID_SCREEN_HOME,
     ID_SCREEN_GAME
 };
