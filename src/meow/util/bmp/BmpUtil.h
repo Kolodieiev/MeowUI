3ec08d94dbf34b04abceec77da927690de1caa23
Kolodieiev <valerij.kolodeev@gmail.com>
2024-04-18 20:21:27 +0300

    Додано опис методів

--- src/meow/util/bmp/BmpUtil.h
+++ src/meow/util/bmp/BmpUtil.h
@@ -10,7 +10,10 @@ namespace meow
     {
 
     public:
+        // Завантажити BMP із SD карти. BMP повинен мати глибину кольору 16 біт, а кодування кольору у форматі 565
         const uint16_t *loadBmp(const char *path_to_bmp, BmpHeader &out_header);
+        
+        // Зберегти зображення до SD карти. Вхідні дані повинні мати глибину кольору 16 біт, а кодування кольору у форматі 565
         bool saveBmp(BmpHeader &header, const uint16_t *buff, const char *path_to_bmp);
 
     private:
