48bf8ab58a30a5eea220dcbfb9275f043aabc835
Kolodieiev <valerij.kolodeev@gmail.com>
2025-01-16 00:22:18 +0200

    Update sd_setup.h

--- include/meowui_setup/sd_setup.h
+++ include/meowui_setup/sd_setup.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-#define SD_SPI_BUS HSPI
+#define SD_SPI_BUS HSPI // Номер шини SPI
 
 #define SD_PIN_CS (int8_t)16
 #define SD_PIN_MISO (int8_t)8
@@ -9,5 +9,5 @@
 #define SD_PIN_SCLK (int8_t)18
 
 #define SD_FREQUENCY 80000000
-#define SD_MOUNTPOINT "/sd"
+#define SD_MOUNTPOINT "/sd" // Не міняй!
 #define SD_MAX_FILES (uint8_t)255
