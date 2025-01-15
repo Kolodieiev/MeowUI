f1153ce2a459de4a5f65c014deeb4c6bb0a7e499
Kolodieiev <valerij.kolodeev@gmail.com>
2025-01-15 18:54:25 +0200

    Update IContext.h

--- src/meow/ui/context/IContext.h
+++ src/meow/ui/context/IContext.h
@@ -128,7 +128,7 @@ namespace meow
     private:
         SemaphoreHandle_t _layout_mutex;
 
-        const uint8_t UI_UPDATE_DELAY = 25; // затримка між фреймами. 40FPS
+        const uint8_t UI_UPDATE_DELAY = 20; // затримка між фреймами
         IWidgetContainer *_layout;
         //
         bool _is_released{false};
