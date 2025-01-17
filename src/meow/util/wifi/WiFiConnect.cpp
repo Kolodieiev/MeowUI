97171c04af438e7115c2d330796009e3d00e1875
Kolodieiev <valerij.kolodeev@gmail.com>
2025-01-17 23:47:29 +0200

    Очистка коду

--- src/meow/util/wifi/WiFiConnect.cpp
+++ src/meow/util/wifi/WiFiConnect.cpp
@@ -1,85 +1,88 @@
 #include "WiFiConnect.h"
 
-meow::WiFiConnect *meow::WiFiConnect::_instance;
-
-meow::WiFiConnect::WiFiConnect()
+namespace meow
 {
-    _instance = this;
-}
+    WiFiConnect *WiFiConnect::_instance;
 
-bool meow::WiFiConnect::tryConnectTo(String ssid, String pwd, int32_t wifi_chan, bool autoreconnect)
-{
-    WiFi.onEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-    WiFi.onEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
-    WiFi.setAutoReconnect(autoreconnect);
-
-    wl_status_t status = WiFi.begin(ssid, pwd, wifi_chan);
-
-    if (status != WL_DISCONNECTED)
+    WiFiConnect::WiFiConnect()
     {
-        log_e("Помилка приєднання до: %s", ssid.c_str());
-        WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-        WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
-        return false;
+        _instance = this;
     }
 
-    return true;
-}
-
-void meow::WiFiConnect::setOnDoneHandler(WiFiConnectHandler handler, void *arg)
-{
-    _onConnectHandler = handler;
-    _onConnectHandlerArg = arg;
-}
-
-void meow::WiFiConnect::setWiFiPower(WiFiPowerLevel power_lvl)
-{
-    switch (power_lvl)
+    bool WiFiConnect::tryConnectTo(String ssid, String pwd, int32_t wifi_chan, bool autoreconnect)
     {
-    case WIFI_POWER_MIN:
-        WiFi.setTxPower(WIFI_POWER_5dBm);
-        break;
-    case WIFI_POWER_MEDIUM:
-        WiFi.setTxPower(WIFI_POWER_15dBm);
-        break;
-    case WIFI_POWER_MAX:
-        WiFi.setTxPower(WIFI_POWER_19_5dBm);
-        break;
+        WiFi.onEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+        WiFi.onEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+        WiFi.setAutoReconnect(autoreconnect);
+
+        wl_status_t status = WiFi.begin(ssid, pwd, wifi_chan);
+
+        if (status != WL_DISCONNECTED)
+        {
+            log_e("Помилка приєднання до: %s", ssid.c_str());
+            WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+            WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+            return false;
+        }
+
+        return true;
     }
-}
 
-void meow::WiFiConnect::disconnect()
-{
-    _onConnectHandler = nullptr;
-    WiFi.disconnect(true);
-    WiFi.mode(WIFI_OFF);
-}
-
-void meow::WiFiConnect::callOnDoneHandler()
-{
-    if (!_onConnectHandler)
+    void WiFiConnect::setOnDoneHandler(WiFiConnectHandler handler, void *arg)
     {
+        _onConnectHandler = handler;
+        _onConnectHandlerArg = arg;
+    }
+
+    void WiFiConnect::setWiFiPower(WiFiPowerLevel power_lvl)
+    {
+        switch (power_lvl)
+        {
+        case WIFI_POWER_MIN:
+            WiFi.setTxPower(WIFI_POWER_5dBm);
+            break;
+        case WIFI_POWER_MEDIUM:
+            WiFi.setTxPower(WIFI_POWER_15dBm);
+            break;
+        case WIFI_POWER_MAX:
+            WiFi.setTxPower(WIFI_POWER_19_5dBm);
+            break;
+        }
+    }
+
+    void WiFiConnect::disconnect()
+    {
+        _onConnectHandler = nullptr;
         WiFi.disconnect(true);
         WiFi.mode(WIFI_OFF);
     }
-    else
-    {
-        log_i("WiFi.status: %d", WiFi.status());
-        _onConnectHandler(_onConnectHandlerArg, WiFi.status());
-    }
-}
 
-void meow::WiFiConnect::onEvent(WiFiEvent_t event)
-{
-    WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-    WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
-
-    if (event == ARDUINO_EVENT_WIFI_STA_GOT_IP)
+    void WiFiConnect::callOnDoneHandler()
     {
-        long unsigned got_ip_time = millis();
-        while (millis() - got_ip_time < 2000 || WiFi.status() != WL_CONNECTED)
-            vTaskDelay(50 / portTICK_PERIOD_MS);
+        if (!_onConnectHandler)
+        {
+            WiFi.disconnect(true);
+            WiFi.mode(WIFI_OFF);
+        }
+        else
+        {
+            log_i("WiFi.status: %d", WiFi.status());
+            _onConnectHandler(_onConnectHandlerArg, WiFi.status());
+        }
     }
 
-    _instance->callOnDoneHandler();
+    void WiFiConnect::onEvent(WiFiEvent_t event)
+    {
+        WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+        WiFi.removeEvent(onEvent, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+
+        if (event == ARDUINO_EVENT_WIFI_STA_GOT_IP)
+        {
+            long unsigned got_ip_time = millis();
+            while (millis() - got_ip_time < 2000 || WiFi.status() != WL_CONNECTED)
+                vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
+
+        _instance->callOnDoneHandler();
+    }
 }
